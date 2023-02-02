bffed0f237d0773c61772df8fe3d15da78e02748
Kamil <kamil@klemczak.pl>
2023-02-02 01:28:04 +0100

    Kontruktor + zapytanie o departament + generator hasla

---
+++ src/main/java/org/emailapp/Email.java
@@ -0,0 +1,59 @@
+package org.emailapp;
+
+import java.util.Scanner;
+
+public class Email {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailboxCapacity;
+    private String alternateEmail;
+
+    //konstruktor dla firstName i lastName/constructor for firstName and lastName
+    public Email(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.println("EMAIL CREATED " +this.firstName + " " + this.lastName);
+
+        //zapytanie iustawienie oddzaiłu/call method asking for dep and return dep
+        this.department = setDepartment();
+        System.out.println("Department: " + this.department);
+
+        //metoda generujaca random haslo/ call method that returns random password
+
+
+    }
+
+    //zapytanie o departament/ask for department
+    private String setDepartment(){
+        System.out.println("DEPARTAMENT CODES:\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter departament code:");
+        Scanner in = new Scanner(System.in);
+        int depChoice = in.nextInt();
+        if (depChoice == 1) { return "Sales"; }
+        else if (depChoice == 2) { return "Development"; }
+        else if (depChoice == 3) { return "Accounting"; }
+        else  { return "" ;}
+
+    }
+
+    //generate random password
+    private String randomPassword(int lenght){
+        String passwordSet = "AaBbCcDdEeFfGgHhIjKkLlMmNnOpRrSsTtUuWwQqXxYyZz1234567890!@#$%^&*()";
+        char[] password = new char[lenght];
+        for (int i=0; i<lenght; i++) {
+            int rand = (int) Math.random() * passwordSet.length();
+            password[i] = passwordSet.charAt(rand);
+        }
+
+
+
+
+    }
+
+
+
+
+}
+
+
---
+++ src/main/java/org/emailapp/EmailApp.java
@@ -0,0 +1,8 @@
+package org.emailapp;
+
+public class EmailApp {
+    public static void main(String[] args) {
+        Email em1 = new Email("John","Smith");
+
+    }
+}
